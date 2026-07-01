9917c13134ace2ea5ad4afa0ea19dd40ebb51c3e
animagroup-vn <animagroupcontact@gmail.com>
2026-07-01 20:13:20 +0700

    feat(core): create navigation constants

---
+++ lib/navigation.ts
@@ -0,0 +1,37 @@
+export const NAVIGATION = [
+  {
+    id: "about",
+    label: "ABOUT",
+    vi: "Giới thiệu",
+  },
+  {
+    id: "philosophy",
+    label: "PHILOSOPHY",
+    vi: "Triết lý",
+  },
+  {
+    id: "ecosystem",
+    label: "ECOSYSTEM",
+    vi: "Hệ sinh thái",
+  },
+  {
+    id: "projects",
+    label: "PROJECTS",
+    vi: "Dự án",
+  },
+  {
+    id: "investors",
+    label: "INVESTORS",
+    vi: "Nhà đầu tư",
+  },
+  {
+    id: "journal",
+    label: "JOURNAL",
+    vi: "Tạp chí",
+  },
+  {
+    id: "contact",
+    label: "CONTACT",
+    vi: "Liên hệ",
+  },
+];
