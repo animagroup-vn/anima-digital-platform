6e917e2c9d6b977d0570a824716b9be18fff8bb9
animagroup-vn <animagroupcontact@gmail.com>
2026-07-01 19:44:15 +0700

    feat(core): create global brand constants

---
+++ lib/brand.ts
@@ -0,0 +1,29 @@
+export const BRAND = {
+  name: "A.N.I.M.A",
+
+  shortName: "ANIMA",
+
+  tagline: "The House of Asset Transformation",
+
+  heroTitle: "THE SOUL OF ASSET DEVELOPMENT",
+
+  heroSubtitle:
+    "Transforming spaces into value. Creating legacy through time.",
+
+  philosophy:
+    "We awaken value where others only see space.",
+
+  website: "https://animagroup.vn",
+
+  email: "contact@animagroup.vn",
+
+  phone: "",
+
+  address: "Ho Chi Minh City, Vietnam",
+
+  colors: {
+    champagne: "#C8A45D",
+    ivory: "#F4F2EC",
+    midnight: "#0A0A0A",
+  },
+};
