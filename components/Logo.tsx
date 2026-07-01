149614f98d128e19bf670b5f5c043799762ef06e
animagroup-vn <animagroupcontact@gmail.com>
2026-07-01 19:50:08 +0700

    refactor(logo): use BRAND constants

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,33 +1,23 @@
+import { BRAND } from "@/lib/brand";
+
 export default function Logo() {
   return (
-    <a href="/" className="group flex items-center gap-4">
-
+    <a href="/" className="group flex items-center gap-3">
       <img
         src="/logo/anima-symbol.png"
-        alt="A.N.I.M.A"
-        className="
-          h-10
-          w-auto
-          object-contain
-          transition-all
-          duration-700
-          group-hover:scale-110
-          group-hover:rotate-[2deg]
-        "
+        alt={BRAND.name}
+        className="h-8 w-auto object-contain transition duration-500 group-hover:scale-105"
       />
 
       <div className="leading-none">
-
-        <div className="font-serif text-base tracking-[.42em] text-ivory">
-          A.N.I.M.A
+        <div className="font-serif text-sm tracking-[0.35em] text-ivory">
+          {BRAND.name}
         </div>
 
-        <div className="mt-1 text-[9px] uppercase tracking-[.32em] text-champagne">
-          The House of Asset Transformation
+        <div className="mt-1 text-[8px] uppercase tracking-[0.28em] text-champagne">
+          Group
         </div>
-
       </div>
-
     </a>
   );
 }
